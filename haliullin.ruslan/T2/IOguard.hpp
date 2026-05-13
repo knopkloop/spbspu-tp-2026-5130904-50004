6332703fb9c05b63566c947c4218e3a9110cea98
knopkloop <hruslan2007@gmail.com>
2026-05-13 22:13:54 +0300

    added IOguard

---
+++ haliullin.ruslan/T2/IOguard.hpp
@@ -0,0 +1,38 @@
+#ifndef IOGUARD_HPP
+#define IOGUARD_HPP
+
+#include <ios>
+
+namespace haliullin
+{
+  class IOguard
+  {
+  public:
+    explicit IOguard(std::basic_ios< char >& s);
+    ~IOguard();
+  private:
+    std::basic_ios< char >& s_;
+    std::streamsize width_;
+    std::streamsize precision_;
+    std::basic_ios< char >::fmtflags fmt_;
+    char fill_;
+  };
+}
+
+haliullin::IOguard::IOguard(std::basic_ios< char >& s):
+  s_(s),
+  width_(s.width()),
+  fill_(s.fill()),
+  precision_(s.precision()),
+  fmt_(s.flags())
+{}
+
+haliullin::IOguard::~IOguard()
+{
+  s_.width(width_);
+  s_.fill(fill_);
+  s_.precision(precision_);
+  s_.flags(fmt_);
+}
+
+#endif
